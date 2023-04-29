5cf5813ad60b3de8b26c344993a96aed1dfb3657
David Kariuki <dkaris.k@gmail.com>
2023-04-29 13:07:07 +0300

    feat: Create ping method
    Create controller method to respond to ping check
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/ApiController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/ApiController.java
@@ -1,7 +1,32 @@
 package com.liniantt.duesclerk.backend_api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 public class ApiController {
+
+    /**
+     * Method to receive ping calls to API
+     *
+     * @return ResponseEntity<?> - Response
+     */
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> ping() {
+        return ResponseEntity.ok(
+                Map.of("response",
+                        Map.of(
+                                "status", HttpStatus.OK,
+                                "error", false,
+                                "timestamp", LocalDateTime.now()
+                        )
+                )
+        );
+    }
 }
