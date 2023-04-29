aa180373c33ca71320dd9924c8cfca4aaac39fc0
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:30:53 +0300

    feat: Create controller class
    Create AnalyticsController.java class

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/AnalyticsController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/AnalyticsController.java
@@ -1,10 +1,3 @@
-/**
- * AnalyticsController class
- *
- * @apiNote Analytics controller class
- * @since April 2023
- */
-
 package com.liniantt.duesclerk.backend_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
