8e02478ee5156dac77190c4ad1c0804149e1948f
David Kariuki <dkaris.k@gmail.com>
2023-04-29 13:00:14 +0300

    feat: Create service implementation class
    Create AnalyticsServiceImpl.java implementation class
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/com/liniantt/duesclerk/backend_api/service_impl/AnalyticsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.liniantt.duesclerk.backend_api.service_impl;
+
+import com.liniantt.duesclerk.backend_api.service.AnalyticsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AnalyticsServiceImpl implements AnalyticsService {
+}
