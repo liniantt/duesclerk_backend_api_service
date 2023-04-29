6f251190088dde3f6e30146d6b705375c0c7bede
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:58:19 +0300

    refactor: Change class to interface
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/service/AnalyticsService.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/service/AnalyticsService.java
@@ -1,9 +1,7 @@
 package com.liniantt.duesclerk.backend_api.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
-public class AnalyticsService {
+public interface AnalyticsService {
 }
