8c9f661f105840279e32545aa641fe3389b86126
David Kariuki (DK) <14153276+david-kariuki@users.noreply.github.com>
2023-04-30 00:22:53 +0300

    feat: Test classes templates
    Create test classes templates
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/test/java/com/liniantt/duesclerk/backend_api/controller/DebtControllerTest.java
@@ -0,0 +1,23 @@
+package com.liniantt.duesclerk.backend_api.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@JsonTest
+@RestClientTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureWebTestClient
+@ExtendWith(SpringExtension.class)
+public class DebtControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+}
---
+++ src/test/java/com/liniantt/duesclerk/backend_api/controller/UserAccountControllerTest.java
@@ -0,0 +1,24 @@
+package com.liniantt.duesclerk.backend_api.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@JsonTest
+@RestClientTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureWebTestClient
+@ExtendWith(SpringExtension.class)
+public class UserAccountControllerTest {
+
+
+  @Autowired
+  private MockMvc mockMvc;
+}
