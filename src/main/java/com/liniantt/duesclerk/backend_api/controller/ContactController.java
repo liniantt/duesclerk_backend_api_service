4e55d9d053a6c00da36ad6d93dd610f3b3258830
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:37:10 +0300

    refactor: Rename class name

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/ContactController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/ContactController.java
@@ -1,8 +1,3 @@
-/**
- * @apiNote ContactController class
- * @since 1.0.0
- */
-
 package com.liniantt.duesclerk.backend_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
