981024ebe56452533bc2ec8a995009aa37bb5ad1
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:37:10 +0300

    refactor: Rename class name

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/DebtController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/DebtController.java
@@ -3,6 +3,6 @@ package com.liniantt.duesclerk.backend_api.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ContactsController {
+public class DebtController {
 
 }
