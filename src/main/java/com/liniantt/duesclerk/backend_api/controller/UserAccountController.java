40387745a220ac93fd73df2c3e7a29cb85b9e20b
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:31:35 +0300

    feat: Create controller class
    Create UserAccountController.java class

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/UserAccountController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/UserAccountController.java
@@ -1,10 +1,3 @@
-/**
- * UserAccountController class
- *
- * @apiNote User account controller class
- * @since April 2023
- */
-
 package com.liniantt.duesclerk.backend_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
