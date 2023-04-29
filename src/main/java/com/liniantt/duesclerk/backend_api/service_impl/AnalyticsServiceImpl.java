0297a03c476956616deb80620048b5e3e154ae15
David Kariuki <dkaris.k@gmail.com>
2023-04-29 13:00:14 +0300

    feat: Create service implementation class
    Create AnalyticsServiceImpl.java implementation class
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/service_impl/AnalyticsServiceImpl.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/service_impl/AnalyticsServiceImpl.java
@@ -1,9 +1,3 @@
-/**
- * @apiNote AnalyticsServiceImpl class
- * @see com.liniantt.duesclerk.backend_api.service.AnalyticsService - Interface class
- * @since 1.0.0
- */
-
 package com.liniantt.duesclerk.backend_api.service_impl;
 
 import com.liniantt.duesclerk.backend_api.service.AnalyticsService;
