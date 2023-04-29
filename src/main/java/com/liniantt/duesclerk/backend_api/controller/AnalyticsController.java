9c3f445248c5d5b61bfaf2742f1c90d7415721bb
David Kariuki <dkaris.k@gmail.com>
2023-04-29 19:12:44 +0300

    docs: Add class-level javadoc documentation
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/AnalyticsController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/AnalyticsController.java
@@ -1,3 +1,10 @@
+/**
+ * AnalyticsController class
+ *
+ * @apiNote Analytics controller class
+ * @since April 2023
+ */
+
 package com.liniantt.duesclerk.backend_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/liniantt/duesclerk/backend_api/controller/DebtController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/DebtController.java
@@ -1,3 +1,10 @@
+/**
+ * DebtController class
+ *
+ * @apiNote Debt controller class
+ * @since April 2023
+ */
+
 package com.liniantt.duesclerk.backend_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
