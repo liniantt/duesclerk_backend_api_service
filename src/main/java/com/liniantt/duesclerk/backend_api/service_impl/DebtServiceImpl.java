9f3a7d3d0c8bb19e275e2632b54036cd4a893491
David Kariuki <dkaris.k@gmail.com>
2023-04-29 13:00:36 +0300

    feat: Create service implementation class
    Create DebtServiceImpl.java implementation class
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/com/liniantt/duesclerk/backend_api/service_impl/DebtServiceImpl.java
@@ -0,0 +1,13 @@
+package com.liniantt.duesclerk.backend_api.service_impl;
+
+import com.liniantt.duesclerk.backend_api.service.DebtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class DebtServiceImpl implements DebtService {
+
+}
