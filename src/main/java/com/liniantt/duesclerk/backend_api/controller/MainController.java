e12874f9d56a477470c96101199ed9b28b62d50d
David Kariuki <dkaris.k@gmail.com>
2023-04-29 13:15:54 +0300

    feat: Create controller class
    Create main controller class with default index method
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.liniantt.duesclerk.backend_api.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(
+            path = {"", "/", ""},
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH,
+                    RequestMethod.HEAD, RequestMethod.DELETE, RequestMethod.OPTIONS, RequestMethod.TRACE},
+            produces = MediaType.TEXT_HTML_VALUE)
+    public String index() {
+        return "index";
+    }
+}
