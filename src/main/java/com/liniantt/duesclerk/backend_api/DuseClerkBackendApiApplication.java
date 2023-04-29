2df4a0dcb9d2b86dd0da3bcdcfc25a3c4a457905
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:22:12 +0300

    refactor: Refactor main class
    Rename main class from BackendApiApplication.java to DuesClerkBackendApiApplication

--- src/main/java/com/liniantt/duesclerk/backend_api/DuseClerkBackendApiApplication.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/DuseClerkBackendApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApiApplication {
+public class DuseClerkBackendApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApiApplication.class, args);
+		SpringApplication.run(DuseClerkBackendApiApplication.class, args);
 	}
 
 }
