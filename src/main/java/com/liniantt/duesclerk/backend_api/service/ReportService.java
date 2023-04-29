ada0c39585e73d8e41d602e01be952f358d1e615
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:53:21 +0300

    feat: Create service interface
    Create ReportService.java interface class
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/service/ReportService.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/service/ReportService.java
@@ -1,14 +1,12 @@
-/**
- * @apiNote ReportService class
- * @see com.liniantt.duesclerk.backend_api.service_impl.ReportServiceImpl - Interface class implementation
- * @since 1.0.0
- */
-
 package com.liniantt.duesclerk.backend_api.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
-public interface ReportService {
+public class ReportService {
 
 }
