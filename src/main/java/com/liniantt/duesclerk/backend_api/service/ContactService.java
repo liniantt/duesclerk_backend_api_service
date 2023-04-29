074630cf0c209f6176f49c48e086dcbb8505413c
David Kariuki <dkaris.k@gmail.com>
2023-04-29 12:56:26 +0300

    refactor: Remove @Sl4j logging annotation
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/service/ContactService.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/service/ContactService.java
@@ -1,10 +1,8 @@
 package com.liniantt.duesclerk.backend_api.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ContactService {
