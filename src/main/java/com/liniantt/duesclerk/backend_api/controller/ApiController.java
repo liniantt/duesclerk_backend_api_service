9399cf23de5777e2ce941ac5233a41d41afee7e9
David Kariuki <dkaris.k@gmail.com>
2023-05-01 11:08:18 +0300

    feat: Request mapping
    Configure endpoint mapping
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/controller/ApiController.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/controller/ApiController.java
@@ -12,11 +12,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@RequestMapping("/api/v1")
 @RestController
 public class ApiController {
 
@@ -25,7 +27,7 @@ public class ApiController {
    *
    * @return ResponseEntity<?> - Response
    */
-  @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(path = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<?> ping() {
     return ResponseEntity.ok(
         Map.of("response",
