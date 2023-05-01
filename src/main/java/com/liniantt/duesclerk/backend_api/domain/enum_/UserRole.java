e9c1793eb6ee653e27031ca81e4b91832ec9cae1
David Kariuki <dkaris.k@gmail.com>
2023-05-01 14:25:57 +0300

    feat: Create user roles enum class
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/com/liniantt/duesclerk/backend_api/domain/enum_/UserRole.java
@@ -0,0 +1,15 @@
+/**
+ * @author David Kariuki
+ * @version 1.0.0
+ * @apiNote UserRole enum
+ * @created 01/05/2023 - 6:52 AM UTC-4
+ * @see
+ * @since 1.0.0
+ */
+
+package com.liniantt.duesclerk.backend_api.domain.enum_;
+
+public enum UserRole {
+  USER,
+  ADMIN
+}
