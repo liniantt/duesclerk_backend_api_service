5df841018a8708dd5fd6b58053002da3a4e7742b
David Kariuki <dkaris.k@gmail.com>
2023-05-01 17:02:27 +0300

    feat: Create user roles enum class
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- src/main/java/com/liniantt/duesclerk/backend_api/domain/enum_/UserRole.java
+++ src/main/java/com/liniantt/duesclerk/backend_api/domain/enum_/UserRole.java
@@ -9,7 +9,32 @@
 
 package com.liniantt.duesclerk.backend_api.domain.enum_;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
-  USER,
-  ADMIN
+
+  ORDINARY_USER("USER"), // Ordinary user
+  ADMINISTRATOR("ADMIN"); // Administrator
+
+  private final String userRole;
+
+  UserRole(String userRole) {
+    this.userRole = userRole;
+  }
+
+  /**
+   * Method to convert UserRole shortname to enum
+   *
+   * @param user - User - shortname
+   * @return UserRole
+   */
+  public static UserRole fromShortName(String user) {
+    return switch (user) {
+      case "USER" -> UserRole.ORDINARY_USER;
+      case "ADMIN" -> UserRole.ADMINISTRATOR;
+      default -> throw new IllegalArgumentException(
+          "Invalid user role : " + user + "!");
+    };
+  }
 }
