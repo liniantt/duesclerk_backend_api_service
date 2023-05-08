df9a3ce2b9c96b5a396d34d337905b1c791bc9f5
David Kariuki <dkaris.k@gmail.com>
2023-05-09 00:52:16 +0300

    feat: Create registration request body
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

---
+++ src/main/java/com/liniantt/duesclerk/backend_api/dto/input/RegisterUserRequestBody.java
@@ -0,0 +1,77 @@
+/**
+ * @apiNote class
+ * @author David Kariuki
+ * @version 1.0.0
+ * @created 06/05/2023
+ * @see
+ * @since 1.0.0
+ */
+package com.liniantt.duesclerk.backend_api.dto.input;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.validation.Valid;
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+  "fullName",
+  "emailAddress",
+  "countryId",
+  "password",
+  "accountType",
+  "createdAtTimestamp",
+  "updatedAtTimestamp",
+  "emailAddressVerified"
+})
+@Generated("jsonschema2pojo")
+public class RegisterUserRequestBody implements Serializable {
+
+  private static final long serialVersionUID = 5605629084403969336L;
+
+  @JsonProperty("fullName")
+  private String fullName;
+
+  @JsonProperty("emailAddress")
+  private String emailAddress;
+
+  @JsonProperty("countryId")
+  private String countryId;
+
+  @JsonProperty("password")
+  private String password;
+
+  @JsonProperty("accountType")
+  private String accountType;
+
+  @JsonProperty("createdAtTimestamp")
+  private String createdAtTimestamp;
+
+  @JsonProperty("updatedAtTimestamp")
+  private String updatedAtTimestamp;
+
+  @JsonProperty("emailAddressVerified")
+  private Boolean emailAddressVerified;
+
+  @JsonIgnore @Valid
+  private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return this.additionalProperties;
+  }
+
+  @JsonAnySetter
+  public void setAdditionalProperty(String name, Object value) {
+    this.additionalProperties.put(name, value);
+  }
+}
